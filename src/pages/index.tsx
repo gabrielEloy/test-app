0f9a53f8328436b3af9b1f4fc56d8a5ac74772a2
Gabriel Eloy <gabrieleloy@Gabriels-MacBook-Pro.local>
2023-05-19 13:02:26 -0300

    added auto logging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,54 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { useState } from 'react'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
-  const [localValue, setLocalValue] = useState<any>(null)
-  const [retrievalsLog, setRetrievalsLog] = useState<any[]>([]);
-  
-  const handleRetrieveValue = () => {
-    const retrievedValue = localStorage.getItem('kronos');
-    setLocalValue(retrievedValue )
-    addToRetrievalsLog({date: new Date(), value: retrievedValue})
-  }
+  const [localValue, setLocalValue] = useState<any>(null);
+  const [logs, setLogs] = useState<any[]>([]);
 
-  const addToRetrievalsLog = (log: any) => {
-    setRetrievalsLog([log, ...retrievalsLog]);
-  }
-  
+  const addToLog = useCallback(
+    (log: any) => {
+      setLogs([log, ...logs]);
+    },
+    [setLogs, logs]
+  );
+
+  const handleRetrieveValue = useCallback(
+    (isAuto?: boolean) => {
+      const retrievedValue = localStorage.getItem("kronos");
+      setLocalValue(retrievedValue);
+      const logData: any = { date: new Date(), value: retrievedValue };
+
+      if (isAuto) {
+        logData.isAuto = isAuto;
+      }
+
+      addToLog(logData);
+    },
+    [addToLog]
+  );
+
+  useEffect(() => {
+    setTimeout(() => handleRetrieveValue(true), 3000)
+  }, [])
+
   return (
     <>
-    <div>
-      <h1>retrieve values test</h1>
-      <button onClick={handleRetrieveValue}>retrieve</button>
-      <p>value: </p>
-      <p>{String(localValue)}</p>
-      <h3>logs</h3>
-      <ul>
-        {retrievalsLog.map(log => <li key={log.date.getTime()}>
-          <span>value: {String(log.value)} - {log.date.toString()}</span>
-        </li>)}
-      </ul>
-    </div>
+      <div>
+        <h1>retrieve values test</h1>
+        <button onClick={() => handleRetrieveValue()}>retrieve</button>
+        <p>value: </p>
+        <p>{String(localValue)}</p>
+        <h3>logs</h3>
+        <ul>
+          {logs.map((log) => (
+            <li key={log.date.getTime()}>
+              <span>
+                {log.isAuto ? "[AUTOMATIC]" : ""} value: {String(log.value)} -{" "}
+                {log.date.toString()}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+  );
 }
